1d38799c028a4f6b60aaf3ea8b5b984e7fcdfed5
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-06-25 12:53:08 +0000

    fcreating use case to findi optimum percent for pb renko

---
+++ renko_trials/renko_trials/use_cases/pb_renko_find_optimum_percent.py
@@ -0,0 +1,28 @@
+from renko_trials.use_cases.pb_renko_create import PBRenkoCreateUseCase
+from decimal import Decimal, getcontext
+
+
+def find_optimum_percent(repo, symbol):
+    found_percentages = []
+    for i in (x / 10 for x in range(9, 101)):
+        pb_renko_create_use_case = PBRenkoCreateUseCase(repo, symbol, i)
+        pb_renko = pb_renko_create_use_case.create_pbrenko()
+
+        if pb_renko.number_of_leaks == 0:
+            found_percentages.append(i)
+
+    if len(found_percentages) == 0:
+        return None
+    elif len(found_percentages) == 1:
+        return found_percentages[0]
+    else:
+        getcontext().prec = 1
+        for i, k in enumerate(found_percentages):
+            all_equal = True
+            for t, z in enumerate(found_percentages[i:]):
+                if i + t != len(found_percentages) - 1:
+                    if (float(Decimal(found_percentages[i + t + 1]) - Decimal(found_percentages[i + t]))) != float(0.1):
+                        all_equal = False
+                        break
+            if all_equal:
+                return k
